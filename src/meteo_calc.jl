5b20c326d9cb245a4ebdd39642caafe6c6525ea2
tcarion <tristan.carion@gmail.com>
2022-08-03 14:52:03 +0200

    add obukhov

--- src/meteo_calc.jl
+++ src/meteo_calc.jl
@@ -1,17 +1,24 @@
-
 """
     $(TYPEDSIGNATURES)
 
-Calculate the friction velocity from the surface pressure `p` [Pa], temperature `t` [K], dewpoint temp `td` [K] and stress `stress` [N/m²].
+Calculate Obukhov scale height from surface meteorological data and sensible heat flux.
+# Arguments                                           
+- ps:      surface pressure [Pa]                  
+- ts:      surface temperature [K]                
+- td:      surface dew point [K]                  
+- t:       temperature first model level [K]
+- p :      pressure first model level [Pa]
+- u_star: scale velocity [m/s]                   
+- q:       surface sensible heat flux [W/m2]      
 """
-function friction_velocity(p, t, td, stress, R_gas)
-    # vapor pressure
+function obukhov(ps, ts, td, t, p, u_star, q, c_p = 1004.5, R_gas = 287.05, karman = 0.4, g = 9.81)
     e = saturation_pressure(td)
-    tv = virtual_temp(p, t, e)
-    rho_moist = p / (R_gas * tv)
-    sqrt(abs(stress) / rho_moist)
+    tv = virtual_temp(ps, t, e)
+    rho = p / (R_gas * tv)
+    θ = potential_temp(t, p, c_p, R_gas)
+    θ_star = friction_temp(q, rho, u_star, c_p)
+    u_star^2 * ts / (karman * g * θ_star)
 end
-
 """
     $(TYPEDSIGNATURES)
 
@@ -33,3 +40,44 @@ end
 Calculate the virtual temperature from the surface pressure `ps` [Pa], the temperature `t` [K], and the vapor pressure `e` [Pa].
 """
 virtual_temp(ps, t, e) = t / (1 - 0.378 * e / ps )
+
+"""
+    $(TYPEDSIGNATURES)
+
+Calculate the potential temperature.
+
+# Arguments                                           
+- t:       temperature [K]                
+- p:       pressure [Pa]                
+- c_p:     specific heat capacities of dry air [J/kg/K]                
+- R_gas:   individual gas constant for dry air [J/kg/K]
+"""
+potential_temp(t, p, akap) = t * (1e5 / p)^(akap)
+potential_temp(t, p, R_gas, c_p) = potential_temp(t, p, R_gas / c_p)
+
+
+"""
+    $(TYPEDSIGNATURES)
+
+Calculate the friction temperature from the surface pressure `p` [Pa], temperature `t` [K], dewpoint temp `td` [K] and stress `stress` [N/m²].
+
+# Arguments                                              
+- q:       surface sensible heat flux [W/m²]
+- rho:     air density [kg/m³]
+- u_star:   scale velocity [m/s]
+- c_p:     specific heat capacities of dry air [J/kg/K]
+"""
+friction_temp(q, rho, u_star, c_p) = - q / (rho * c_p * u_star)
+
+"""
+    $(TYPEDSIGNATURES)
+
+Calculate the friction velocity from the surface pressure `p` [Pa], temperature `t` [K], dewpoint temp `td` [K] and stress `stress` [N/m²].
+"""
+function friction_velocity(p, t, td, stress, R_gas)
+    # vapor pressure
+    e = saturation_pressure(td)
+    tv = virtual_temp(p, t, e)
+    rho_moist = p / (R_gas * tv)
+    sqrt(abs(stress) / rho_moist)
+end
--- test/meteo_calc.jl
+++ test/meteo_calc.jl
@@ -1,7 +1,10 @@
-import Flexpart: saturation_pressure, virtual_temp, friction_velocity
+import Flexpart: saturation_pressure, virtual_temp, friction_velocity, potential_temp, friction_temp, obukhov
+import Flexpart: Parameters
 using Test
 
 @testset "fluid properties" begin
+    cp = 1004.5
+    R_gas = 287.05
     ew = saturation_pressure(273.15 + 50.) * 1e-3
     @test ew ≈ 12.3440 atol = 1e-1
 
@@ -12,6 +15,19 @@ using Test
 
     e = saturation_pressure(t2)
     tv = virtual_temp(ps, t2, e)
-    u_start = friction_velocity(ps, t2, td2, stress, 287.05)
-    @test u_start ≈ 0.138913378 atol = 1e-5
+    u_star = friction_velocity(ps, t2, td2, stress, 287.05)
+    @test u_star ≈ 0.138913378 atol = 1e-5
+    
+    p1 = 99804.1719
+    t1 = 291.607758
+    θ = potential_temp(291.607758, p1, R_gas, cp)
+    @test θ ≈ 291.77113 atol = 1e-4
+
+    q = 6.3768158
+    rho = ps / (R_gas * tv)
+    θ_star = friction_temp(q, rho, u_star, cp)
+    @test θ_star ≈ -0.0383189023 atol = 1e-4
+
+    L = obukhov(ps, t2, td2, t1, p1, u_star, q)
+    @test L ≈ -37 atol = 1
 end
