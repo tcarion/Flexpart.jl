8fc3bcc98d1a56f3a73fd9aded03cb9a150cd71e
tcarion <tristancarion@gmail.com>
2022-09-30 11:49:53 +0100

    fix docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,19 @@
-using Documenter, Flexpart
+using Flexpart
+using Documenter
 
-makedocs(
-    sitename="Flexpart.jl docs",
+DocMeta.setdocmeta!(Flexpart, :DocTestSetup, :(using Flexpart); recursive=true)
+
+makedocs(;
+    modules=[Flexpart],
+    authors="tcarion <tristancarion@gmail.com> and contributors",
+    repo="https://github.com/tcarion/Flexpart.jl/blob/{commit}{path}#{line}",
+    sitename="Flexpart.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://tcarion.github.io/Flexpart.jl",
+        edit_link="main",
+        assets=String[],
+    ),
     pages = [
         "Home" => "index.md",
         "Manual" => "man/guide.md",
@@ -9,4 +21,9 @@ makedocs(
             "Internals" => "lib/internals/flexpart.md"
         ],
     ],
-    )
+)
+
+deploydocs(;
+    repo="github.com/tcarion/Flexpart.jl",
+    devbranch="main",
+)
